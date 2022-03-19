3e3bdfdf494ec36577ed33aa6fef467146ae2445
Nico <nico.enking@gmail.com>
2022-03-19 22:14:50 +0100

    Fix adduser command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ if ! id "$DOCKER_USER" >/dev/null 2>&1; then
     echo "Starting with $USER_ID:$GROUP_ID (UID:GID)"
 
     addgroup --gid $GROUP_ID $DOCKER_GROUP
-    adduser -s /bin/sh --uid $USER_ID --gid $DOCKER_GROUP -D $DOCKER_USER
+    adduser -s /bin/sh --uid $USER_ID --ingroup $DOCKER_GROUP -D $DOCKER_USER
 
     chown -vR $USER_ID:$GROUP_ID /opt/minecraft
     chmod -vR ug+rwx /opt/minecraft
